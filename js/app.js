345d4dea71959493a1714c2f43a734435946be6d
qixiaohui <qixiaohuihaha@gmail.com>
2015-11-08 15:07:07 -0800

    first commit

---
+++ js/app.js
@@ -0,0 +1,10 @@
+// create our angular app and inject ngAnimate and ui-router 
+// =============================================================================
+var app=angular.module('lolInfi', ['ngAnimate', 'ui.router']);
+
+// our controller for the form
+// =============================================================================
+app.controller('formController', function($scope,$location,$http) {
+
+    
+});
---
+++ js/config.js
@@ -0,0 +1,36 @@
+// configuring our routes 
+// =============================================================================
+app.config(function($stateProvider, $urlRouterProvider) {
+    
+    $stateProvider
+    //route to show the main page
+    //base page
+    .state('base',{
+        url: '/base',
+        templateUrl: 'templates/base.html',
+        controller: 'formController'
+    })
+    .state('base.home', {
+        url: '/baseHome',
+        template: '<lol-home></lol-home>'
+    })
+    .state('base.champions', {
+        url: '/baseChampions',
+        template:'<lol-champions></lol-champions>'
+    })
+    .state('base.items', {
+        url: '/baseItems',
+        template: '<lol-items></lol-items>'
+    })
+    .state('base.rank', {
+        url: '/baseRank',
+        template: '<lol-rank></lol-rank>'
+    })
+    .state('base.live',{
+        url:'/baseLive',
+        template: '<lol-live></lol-live>'
+    })
+    ;
+    $urlRouterProvider.otherwise('/base');
+    
+});
