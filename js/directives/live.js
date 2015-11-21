5727ab0f572f704ef4aca9becea877155f18953e
qixiaohui <qixiaohuihaha@gmail.com>
2015-11-20 19:50:54 -0800

    improve twitch player UI

--- js/directives/live.js
+++ js/directives/live.js
@@ -9,16 +9,35 @@ app.directive('lolLive',function(){
         replace:true,
         scope:false,
         link:function(scope,element,attrs){
+			scope.index = 1;
 			$("body").css("background-image","url('../images/otherbg.jpg')");
+            //get twitch lol game play list
+            Twitch.api({method: 'streams', params: {game:'League of Legends', limit:21} }, function(error, list) {
+              console.debug(JSON.stringify(list));
+              scope.twitchPlayList = list;
+              //not inisde angular scope need to refresh
+              scope.$apply();
+            });
         },
         controller:function($scope,redirect,videoPlayer){
-            //get twitch lol game play list
-            Twitch.api({method: 'streams', params: {game:'League of Legends', limit:20} }, function(error, list) {
-              console.debug(JSON.stringify(list));
-              $scope.twitchPlayList = list;
-              //not inisde angular scope need to refresh
-              $scope.$apply();
-            });
+
+			
+			$scope.loadPage = function(index){
+				var offset = index*21;
+				$scope.index = index;
+				Twitch.api({method: 'streams'
+							, params: {game:'League of Legends'
+							, limit:21, offset: offset} }
+				, function(error, list) {
+				  console.debug(JSON.stringify(list));
+				  $scope.twitchPlayList = list;
+				  window.scrollTo(0,0);
+				  //not inisde angular scope need to refresh
+				  scope.$apply();
+				});
+			}
+				
+						   
 
             $scope.playVideo = function(info){
                 videoPlayer.setVideo(info);
