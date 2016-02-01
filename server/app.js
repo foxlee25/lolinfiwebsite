5bf77cafe46c330bd6df6213ff14725a8bdec629
qixiaohui <qixiaohuihaha@gmail.com>
2016-02-01 11:03:14 -0800

    Update app.js

--- server/app.js
+++ server/app.js
@@ -1,42 +1,43 @@
-'use strict'
+(function() {
+    'use strict';
 
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
-var compress = require('compression');
-var cors = require('cors');
-var logger = require('morgan');
-var request = require('request');
-var port = 8080;
-var routes;
-var lolStaticApi;
-var lolSummonerApi;
+    var express = require('express');
+    var app = express();
+    var bodyParser = require('body-parser');
+    var compress = require('compression');
+    var cors = require('cors');
+    var logger = require('morgan');
+    var port = 8080;
+    var routes;
+    var lolStaticApi;
+    var lolSummonerApi;
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
-app.use(compress());
-app.use(cors());
-app.use(logger('dev'));
+    app.use(bodyParser.urlencoded({extended: true}));
+    app.use(bodyParser.json());
+    app.use(compress());
+    app.use(cors());
+    app.use(logger('dev'));
 
-lolStaticApi = require('./dao/lol_static_api');
-console.log(lolStaticApi);
-lolSummonerApi = require('./dao/lol_summoner_api');
-routes = require('./route/index')(app, lolStaticApi, lolSummonerApi);
+    lolStaticApi = require('./dao/lol_static_api');
+    console.log(lolStaticApi);
+    lolSummonerApi = require('./dao/lol_summoner_api');
+    routes = require('./route/index')(app, lolStaticApi, lolSummonerApi);
 
 
-console.log('about to launch node server');
+    console.log('about to launch node server');
 
-app.get('/ping',function(req, res){
-	console.log(req.body);
-	res.send('pong');
-});
+    app.get('/ping',function(req, res){
+        console.log(req.body);
+        res.send('pong');
+    });
 
-app.use(express.static('../client/'));
-app.use('/*', express.static('../client/index.html'));
+    app.use(express.static('../client/'));
+    app.use('/*', express.static('../client/index.html'));
 
-app.listen(port,function(req, res){
-	console.log('express module listening on port'+port);
-});
+    app.listen(port,function(){
+        console.log('express module listening on port'+port);
+    });
+}());
 
 
 
