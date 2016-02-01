8af669722232c7cca0c199e297e6bf386bab655a
qixiaohui <qixiaohuihaha@gmail.com>
2016-02-01 10:58:42 -0800

    Update service.js

--- client/js/service.js
+++ client/js/service.js
@@ -24,8 +24,8 @@ app.factory('RiotApi',function($http){
 		* which query to run
 		*/
         getInfo:function(genre){
-            return $http.get('http://localhost:8080/LolInfi/LolStatic'
-							 ,{headers:{
+            return $http.get('http://localhost:8080/LolInfi/LolStatic',
+                             {headers:{
 							 'genre': genre
 							 }});
         },
@@ -35,8 +35,8 @@ app.factory('RiotApi',function($http){
         getChallengerInfo:function(){
             return $http.get('json/challenger_info.json');
         }
-    }
-})
+    };
+});
 
 /**
 * mock call rest summoner api
@@ -44,14 +44,14 @@ app.factory('RiotApi',function($http){
 app.factory('getSummoner',function($http){
 	return{
 		getChampion:function(id, genre){
-			return $http.get('http://localhost:8080/LolInfi/LolApi/SummonerApi'
-							 ,{headers:{
+			return $http.get('http://localhost:8080/LolInfi/LolApi/SummonerApi',
+                             {headers:{
 							 'summoner': id,
 							 'genre': genre
 							 }});
 		}
-	}
-})
+	};
+});
 
 /**
 * call riot rest summoner api
@@ -63,126 +63,128 @@ app.service('RiotSummonerApi',function($http){
 	
 	this.setMatchId = function(input){
 		this.matchId = input;
-	}
+	};
 	
 	this.getMatchId = function(){
 		return this.matchId;
-	}
+	};
 	
 	this.setSummonerId = function(input){
 		this.summonerId = input;
-	}
+	};
 	
 	this.getSummonerId = function(){
 		return this.summonerId;
-	}
+	};
 	
 	this.setSummonerName = function(input){
 		this.summonerName = input;
-	}
+	};
 	
 	this.getSummonerName = function(){
 		return this.summonerName;
-	}
+	};
 	
 	/**
 	* get summoner general info by summoner id
 	*/
 	this.getChampionGeneral = function(){
-		return $http.get('http://localhost:8080/LolInfi/LolSummoner'
-							 ,{headers:{
+		return $http.get('http://localhost:8080/LolInfi/LolSummoner',
+                            {headers:{
 							 'genre': "general",
 							 'id': this.summonerId
 							 }});
-	}
+	};
 	
 	/**
 	* get summoner id from name
 	*/
 	this.getChampionGeneralByName = function(){
-		return $http.get('http://localhost:8080/LolInfi/LolSummoner'
-							 ,{headers:{
+		return $http.get('http://localhost:8080/LolInfi/LolSummoner',
+                            {headers:{
 							 'genre': "getid",
 							 'id': this.summonerName
 							 }});
-	}
+	};
 	
 	/**
 	* get summoner played chapmion list
 	*/
 	this.getChampionRank = function(){
-		return $http.get('http://localhost:8080/LolInfi/LolSummoner'
-							 ,{headers:{
+		return $http.get('http://localhost:8080/LolInfi/LolSummoner',
+                            {headers:{
 							 'genre': "champion",
 							 'id': this.summonerId
 							 }});
-	}
+	};
 	
 	/**
 	* get summoner played matchlist
 	*/
 	this.getMatchList = function(){
-		return $http.get('http://localhost:8080/LolInfi/LolSummoner'
-							 ,{headers:{
+		return $http.get('http://localhost:8080/LolInfi/LolSummoner',
+                            {headers:{
 							 'genre': "matchlist",
 							 'id': this.summonerId
 							 }});
-	}
+	};
 	
 	/**
 	* get match detail by match id
 	*/
 	this.getMatchDetail = function(){
-		return $http.get('http://localhost:8080/LolInfi/LolSummoner'
-							 ,{headers:{
+		return $http.get('http://localhost:8080/LolInfi/LolSummoner',
+                            {headers:{
 							 'genre': "matchdetail",
 							 'id': this.matchId
 							 }});
-	}
-})
+	};
+});
 
 app.service('videoPlayer',function(){
     this.video = null;
     this.setVideo = function(input){
         this.video = input;
-    }
+    };
+    
     this.getVideo = function(){
         return this.video;
-    }
-})
+    };
+});
 
 app.service('championDetail',function($http){
 	this.id = null;
 	this.setChampDetailId = function(input){
 		this.id = input;
-	}
+	};
+    
 	this.getChampDetail = function(genre){
-		return $http.get('http://localhost:8080/LolInfi/LolStatic'
-						 ,{headers:{
+		return $http.get('http://localhost:8080/LolInfi/LolStatic',
+                         {headers:{
 						 'genre': genre,
 						 'id':this.id
 						 }});
-	}
-})
+	};
+});
 
 app.service('loadSummoner',function(){
 	this.summoner = null;
 	this.setSummoner = function(input){
 		this.summoner = input;
-	}
+	};
 	
 	this.getSummoner = function(){
 		return this.summoner;
-	}
-})
+	};
+});
 
 /**
 * Change window location
 */
-app.factory('redirect',function($location,$rootScope){
+app.factory('redirect',function($location){
     return function(path){
         $location.path(path);
-    }
+    };
 });
 
 /**
@@ -190,7 +192,7 @@ app.factory('redirect',function($location,$rootScope){
 */
 app.service('videoUrl',function(){
     
-})
+});
 
 /**
 * utility 
@@ -205,5 +207,5 @@ app.service('util',function(){
 			}
 		}
 		return size;
-	}
-})
+	};
+});
