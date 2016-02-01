083b207e31d591d03922a36a2fdc5f37558bcfe8
qixiaohui <qixiaohuihaha@gmail.com>
2016-02-01 10:58:59 -0800

    Update filter.js

--- client/js/filter.js
+++ client/js/filter.js
@@ -1,8 +1,9 @@
 app.filter('range', function() {
   return function(val, range) {
     range = parseInt(range);
-    for (var i=0; i<range; i++)
+    for (var i=0; i<range; i++){
       val.push(i);
+    }
     return val;
   };
 });
