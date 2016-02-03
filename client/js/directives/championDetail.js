3060a04dfe6be3caa1c2517a8ec0799269ea20cd
Jin Gan <jingan@gj835.local>
2016-02-02 22:08:52 -0800

    edit background image setting; add level slider bar

--- client/js/directives/championDetail.js
+++ client/js/directives/championDetail.js
@@ -2,6 +2,12 @@
 *champion detail
 */
 app.directive('lolChampiondetail',function(){
+    function load_script() {
+        var s = document.createElement('script');
+        s.src = "js/simple-slider.js";
+        document.head.appendChild(s);
+    }
+
     return{
         restrict:'E',
         templateUrl:'templates/base/championDetail.html',
@@ -9,6 +15,18 @@ app.directive('lolChampiondetail',function(){
         replace:true,
         scope:false,
         link:function(scope,element,attrs){
+            load_script();
+            $("#data-slider")
+            .each(function () {
+                console.log("Hello");
+                var input = $(this);
+                $("<span>").addClass("output").insertAfter($(this));
+            })
+            .bind("slider:ready slider:changed", function (event, data) {
+                $(this)
+                .nextAll(".output:first")
+                .html(data.value.toFixed() + "-18");
+            });
         },
         controller:function($scope,redirect,championDetail){
 			 championDetail.getChampDetail("champion_detail")
