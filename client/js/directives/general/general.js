fdc2fd8354e0e9449d831427e3b355d5a1d7d6c2
Doug44444444 <deewang1983@gmail.com>
2016-02-26 14:40:26 -0800

    Update summoner_general_page.html
    1. fixed Last 20 Game CSS
    2. fixed spider chart

--- client/js/directives/general/general.js
+++ client/js/directives/general/general.js
@@ -25,6 +25,7 @@ app.directive('lolChampionsGeneral',function(){
 
 			RiotSummonerApi.getInfo('recentGames')
                 .success(function(data){
+                console.log(JSON.stringify(data));
                 })
                 .error(function(e){
                     console.error(e + " can't get match detail");
@@ -83,7 +84,7 @@ app.directive('lolChampionsGeneral',function(){
                         backgroundColor:'transparent'
                     },       
                     pane: {
-                        size: '65%'
+                        size: '65%',
                     },
                     xAxis: {
                         categories: ['Damage', 'Gold', 'Team Fight', 'Vision Control',
@@ -91,9 +92,9 @@ app.directive('lolChampionsGeneral',function(){
                         tickmarkPlacement: 'on',
                         labels: {
                             style: {
-                                color: 'white',
+                                color: '#cccccc',
                                 fontWeight: 'bold',
-                                fontSize:'15px'
+                                fontSize:'12px'
                             }
                         },
                         lineWidth: 0
@@ -102,7 +103,7 @@ app.directive('lolChampionsGeneral',function(){
                         gridLineInterpolation: 'polygon',
                         lineWidth: 0,
                         min: 0,
-                        max:5,
+                        max: 3,
                         tickInterval:1,
                         labels: {
                             enabled: false
@@ -117,7 +118,9 @@ app.directive('lolChampionsGeneral',function(){
                     },
                     series: [{
                         name: 'Score',
-                        data: [1.5, 2, 3, 4.5, 3.5],
+                        type: 'area',
+                        color: '#77b0ce',
+                        data: [1.5, 1.9, 2.1, 2.7, 2.3],
                         pointPlacement: 'on'
                     }]
                 });
